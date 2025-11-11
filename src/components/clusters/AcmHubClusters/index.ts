6216b0b08355d60f684a44e93b07ceaa1b9a7b08
Erik Jan de Wit <erikjan.dewit@gmail.com>
2025-11-11 16:15:18 +0100

    initial version of the "tag acm cluster" action
    Signed-off-by: Erik Jan de Wit <erikjan.dewit@gmail.com>

---
+++ src/components/clusters/AcmHubClusters/index.ts
@@ -0,0 +1,3 @@
+export { default as AcmHubClusterList } from './AcmHubClusterList';
+export { default as AcmHubEmptyState } from './AcmHubEmptyState';
+export { default as TagAcmHubDialog } from './TagAcmHubDialog';
---
+++ src/queries/AcmHubQueries/useTagAcmHub.ts
@@ -0,0 +1,47 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+import { ACM_HUB_PROPERTY_KEY, ACM_HUB_PROPERTY_VALUE } from '~/common/acmHubConstants';
+import { getClusterServiceForRegion } from '~/services/clusterService';
+
+import { formatErrorData } from '../helpers';
+
+type TagAcmHubParams = {
+  clusterID: string;
+  region?: string;
+  tag: boolean;
+};
+
+export const useTagAcmHub = () => {
+  const queryClient = useQueryClient();
+
+  const { isSuccess, error, isError, isPending, mutate, reset } = useMutation({
+    mutationKey: ['clusterService', 'tagAcmHub'],
+    mutationFn: ({ clusterID, region, tag }: TagAcmHubParams) => {
+      const clusterService = getClusterServiceForRegion(region);
+
+      const properties: { [key: string]: string } = {};
+      if (tag) {
+        properties[ACM_HUB_PROPERTY_KEY] = ACM_HUB_PROPERTY_VALUE;
+      } else {
+        // To remove a property, set it to empty string
+        properties[ACM_HUB_PROPERTY_KEY] = '';
+      }
+
+      return clusterService.editCluster(clusterID, { properties });
+    },
+    onSuccess: () => {
+      // Invalidate cluster queries to refresh the data
+      queryClient.invalidateQueries({ queryKey: ['FETCH_CLUSTERS_QUERY_KEY'] });
+      queryClient.invalidateQueries({ queryKey: ['clusterDetails'] });
+    },
+  });
+
+  return {
+    isSuccess,
+    error: isError && error ? formatErrorData(isPending, isError, error)?.error : null,
+    isError,
+    isPending,
+    mutate,
+    reset,
+  };
+};
